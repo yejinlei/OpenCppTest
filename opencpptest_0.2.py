130c1491991a259dd0245730967b8c6b191177cf
4010896@qq.com <4010896@qq.com>
2018-05-15 01:00:23 +0800

    ver2.0

--- opencpptest_0.2.py
+++ opencpptest_0.2.py
@@ -1,8 +1,7 @@
 #coding:UTF-8
 """
-windows 64bit， python2.7 64bit
+windows 64bit， python2.7 32bit
 """
-
 """test1.c样本
 #include<stdio.h>
 void test1(int x1, int x2){
@@ -25,37 +24,35 @@ m = s.model()
 inputs.append(m)
 
 
-import sys
+import sys,os
 sys.path.append(os.path.join(os.getcwd(), 'clang'))
-from clang import cindex #llvm3.7
-cindex.Config.set_library_path(os.path.join(os.getcwd(), 'clang'))
+from clang import cindex #llvm3.7 32bit
+cindex.Config.set_library_file(os.path.join(os.getcwd(), r'clang/libclang.dll'))
 def dumpnode(node, indent):
     if node.kind == cindex.CursorKind.FUNCTION_DECL:
-        print ' '*indent, '{},{},{},{},{}'.format(str(node.kind), node.spelling, node.location, node.displayname, node.result_type.kind)
+        #print ' '*indent, '{},{},{},{},{}'.format(str(node.kind), node.spelling, node.location, node.displayname, node.result_type.kind)
         typekind = {"TypeKind.VOID":"void"}
-        return node.spelling, typekind[str(node.result_type.kind)+" "+str(node.displayname)]
+        return str(node.spelling), typekind[str(node.result_type.kind)]+" "+node.displayname
 
-    for i in node.get_children():  #TODO 卡死,换了很多版本llvm问题依旧
-        dumpnode(i, indent+2)
+    for i in node.get_children():
+        return dumpnode(i, indent+2)
 
 index = cindex.Index.create()
 path = os.path.join(os.getcwd(), r'test1.c')
 tu = index.parse(path)
 
-template_include = r'#include "gtest/gtest.h"'
-template_decls = r''
-template_tests = """TEST({0}, {1}){
-    {2}({3}, {4});
+template_include = '#include "gtest/gtest.h"\n\n'
+template_decls = ''
+template_tests ="""TEST(%s, %s){
+    %s(%s
 }
 
 """
 funname, template_decls = dumpnode(tu.cursor, 0)
-
-
 def write_unittest():
     with open(os.path.join(os.path.join(os.getcwd(), 'unittest'), 'test_{}.c'.format('test')), 'w+') as f:
-        f.write(template_include+''+template_decls+'')
+        f.write(template_include+template_decls+';\n')
         for num, para in enumerate(inputs):
-            f.write(template_tests.format(funname.upper(), str(num), funname, para[0], para[1]+''))
+            f.write(template_tests % (funname.upper(), str(num), funname, str(para[0]), str(para[1])))
 
 write_unittest()
---
+++ unittest/test_test.c
@@ -0,0 +1,7 @@
+#include "gtest/gtest.h"
+
+void test1(int, int);
+TEST(TEST1, 0){
+    test1(x2, x1);
+}
+
